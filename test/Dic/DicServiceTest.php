5597b579625127bfdde6bead6d843069f8a993a7
sibowen <1265368338@qq.com>
2020-05-30 15:27:02 +0800

    丰富单元测试

--- test/Dic/DicServiceTest.php
+++ test/Dic/DicServiceTest.php
@@ -22,22 +22,22 @@ class DicServiceTest extends HttpTestCase
         $wordService = $this->getContainerWithtrue()->get(\App\Service\Word\WordServiceImplV1::class);
 
         $this->assertEquals(true, $db2Dic);
-        $match = $sentence->match('顺义是北京的一个地区', 'zg', 'main');
-        $this->assertArraySubset($match, ['顺义', '北京', '区域']);
+        $match = $sentence->match('顺义是北京的一个区域', 'zg', 'main');
+        $this->assertArraySubset(['顺义', '北京'], $match, true);
 
         $match = $sentence->match('顺义是北京的一个地区', 'zg', 'notmain');
         $this->assertCount(0, $match);
         $this->assertEquals([], $match);
 
         $find = $wordService->find('北',  'zg', 'main');
-        $this->assertArraySubset($find, ['北京']);
+        $this->assertArraySubset(['北京'], $find);
 
         $find = $wordService->find('域',  'zg', 'main');
         $this->assertCount(0, $find);
         $this->assertEquals([], $find);
 
         $find = $wordService->find('区',  'zg', 'main');
-        $this->assertArraySubset($find, ['区域']);
+        $this->assertArraySubset(['区域'], $find);
 
     }
     /**
@@ -46,11 +46,6 @@ class DicServiceTest extends HttpTestCase
     protected function getContainerWithtrue()
     {
         $container = Mockery::mock(Container::class);
-
-        $words = [];
-        $words[] = ['word'=>'北京', 'from_system'=>'zg', 'type'=>'main'];
-        $words[] = ['word'=>'区域', 'from_system'=>'zg', 'type'=>'main'];
-        $words[] = ['word'=>'顺义', 'from_system'=>'zg', 'type'=>'main'];
 
         $wordDaoStub = $this->createMock(\App\Dao\Word\WordDao::class);
         $wordDaoStub->method('list')->will($this->returnCallback([$this, 'getWord']));
@@ -83,4 +78,66 @@ class DicServiceTest extends HttpTestCase
         $words[] = ['word'=>'顺义', 'from_system'=>'zg', 'type'=>'main'];
         return $words;
     }
+
+    public function getWordAgain($where, $pageStart, $pageNum)
+    {
+        if ($pageStart > $pageNum) {
+            return [];
+        }
+        $words = [];
+        $words[] = ['word'=>'北京', 'from_system'=>'zg', 'type'=>'main'];
+        $words[] = ['word'=>'区域', 'from_system'=>'zg', 'type'=>'main'];
+        $words[] = ['word'=>'顺义', 'from_system'=>'zg', 'type'=>'main'];
+        $words[] = ['word'=>'东北', 'from_system'=>'zg', 'type'=>'main'];
+        $words[] = ['word'=>'songhuajiang', 'from_system'=>'zg', 'type'=>'main'];
+
+        return $words;
+    }
+
+
+    public function testDicRelease()
+    {
+        $wordDaoStub = $this->createMock(\App\Dao\Word\WordDao::class);
+        $wordDaoStub->method('list')->will($this->returnCallback([$this, 'getWord']));
+        $wordStorage = new \App\Util\Dic\WordMemory();
+
+        $sentence = new \App\Service\Sentence\SentenceServiceImplV1($wordStorage);
+        $wordService = new \App\Service\Word\WordServiceImplV1($wordStorage);
+        $dic = new \App\Service\Dic\DicServiceImplV1($wordStorage, $wordDaoStub);
+        //第一次数据录入
+        $this->assertEquals(true, $dic->db2Dic());
+
+        $match = $sentence->match('顺义是北京的一个区域', 'zg', 'main');
+        $this->assertArraySubset(['顺义', '北京', '区域'], $match);
+
+        $match = $sentence->match('顺义是北京的一个地区', 'zg', 'notmain');
+        $this->assertCount(0, $match);
+        $this->assertEquals([], $match);
+
+        $find = $wordService->find('北',  'zg', 'main');
+        $this->assertArraySubset(['北京'], $find, true);
+
+        $find = $wordService->find('东',  'zg', 'main');
+        $this->assertArraySubset($find, [],true);
+
+        //覆盖索引
+        $wordDaoStub = $this->createMock(\App\Dao\Word\WordDao::class);
+        $wordDaoStub->method('list')->will($this->returnCallback([$this, 'getWordAgain']));
+        $dic = new \App\Service\Dic\DicServiceImplV1($wordStorage, $wordDaoStub);
+        $dic->releaseDb2Dic();
+
+        $match = $sentence->match('顺义是北京的一个地区', 'zg', 'main');
+        $this->assertArraySubset(['顺义', '北京'], $match, true);
+
+        $match = $sentence->match('我的家在东北songhuajiang', 'zg', 'main');
+        $this->assertArraySubset(['东北', 'songhuajiang'], $match, true);
+
+        $find = $wordService->find('songhua',  'zg', 'main');
+        $this->assertArraySubset(['songhuajiang'], $find,true);
+
+        $find = $wordService->find('东',  'zg', 'main');
+        $this->assertArraySubset($find, ['东北']);
+    }
+
+
 }
